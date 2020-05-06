8fce6cfbd833779ebe28536cd53c2d2a29dab705
Yoshihara Daiki <daiki.ph.fx@gmail.com>
2020-05-07 06:23:05 +0900

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by 吉原　大輝 on 2020/05/07.
+//  Copyright © 2020 吉原　大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
